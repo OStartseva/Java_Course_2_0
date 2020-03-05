a14a1b7463af1cb4eaa2d3f3f5d5be7b55c0c86f
olgastar888@gmail.com <Wwwwwww!11111>
2020-03-06 00:06:37 +0300

    Calculator + longestWord

--- src/com/company/calculator/Calculator.java
+++ src/com/company/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package com.company.calculator;
 
-import java.util.Scanner;
+
+import java.util.*;
 
 public class Calculator {
     public static void main(String[] args) {
@@ -9,6 +10,20 @@ public class Calculator {
         Scanner scanner1 = new Scanner(System.in);
 
         //Присваиваем переменной первое введенное значение
+        System.out.println("1: Запустить выполнение калькулятора \n" +
+                "2: Поиск максимального слова в массиве");
+        int action = scanner1.nextInt();
+
+        if (action == 1) {
+            calculator();
+        } else if (action == 2) {
+            findLongestWord();
+        }
+    }
+
+    public static void calculator() {
+        Scanner scanner1 = new Scanner(System.in);
+
         System.out.println("Введи первое дробное число: ");
         double first = scanner1.nextDouble();
 
@@ -16,23 +31,21 @@ public class Calculator {
         Scanner scanner2 = new Scanner(System.in);
         String operator = scanner2.nextLine();
 
-
         //Присваиваем переменной второе введенное значение
         System.out.println("Введи второе дробное число: ");
         double second = scanner1.nextDouble();
 
 
-        System.out.printf("%.4f", calculate(first, second, operator) );
-
+        System.out.printf("%.4f", calculate(first, second, operator));
     }
 
     /**
-     * @param first first double
+     * @param first  first double
      * @param second second double
      * @return calculated value
      */
 
-    public static double calculate(Double first, Double second, String operator){
+    public static double calculate(Double first, Double second, String operator) {
 
         switch (operator) {
             case "*":
@@ -44,7 +57,25 @@ public class Calculator {
             default:
                 return first - second;
         }
+    }
 
+    public static void findLongestWord() {
+        System.out.println("Введи размер массива: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String[] arr = new String[scanner.nextInt()];
+
+        Scanner scanner1 = new Scanner(System.in);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner1.nextLine();
+        }
+
+        System.out.println(longestWord(arr));
+    }
+
+    public static String longestWord(String[] arr) {
+        return Arrays.stream(arr).max(Comparator.comparing(String::length)).get();
     }
 }
 
